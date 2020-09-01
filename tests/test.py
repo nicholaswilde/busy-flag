f8ee554855494df297aa3f86a572b5c084ee5194
Nicholas Wilde <ncwilde43@gmail.com>
2020-09-01 06:33:52 +0100

    Updated a bunch of files

--- tests/test.py
+++ tests/test.py
@@ -3,14 +3,21 @@
 import time
 from adafruit_servokit import ServoKit
 
+
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+CHANNEL = 16
 
-kit.servo[0].angle = 180
-kit.continuous_servo[1].throttle = 1
-time.sleep(1)
-kit.continuous_servo[1].throttle = -1
-time.sleep(1)
-kit.servo[0].angle = 0
-kit.continuous_servo[1].throttle = 0
+kit = ServoKit(channels=CHANNEL)
+
+def main():
+	kit.servo[0].angle = 180
+	kit.continuous_servo[1].throttle = 1
+	time.sleep(1)
+	kit.continuous_servo[1].throttle = -1
+	time.sleep(1)
+	kit.servo[0].angle = 0
+	kit.continuous_servo[1].throttle = 0
+
+if __name__ == '__main__':
+	main()
